89e984fab6ace3e116ed134972e67cc2d0eea1a4
Michael Kim <m.kim4247@gmail.com>
2018-10-04 14:57:32 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -97,29 +97,6 @@ def organize_schools(schools)
     org_schools
 end 
 
-   #schools = hash of hashes of hashes
-   #school's name is first key and value is another hash with key (:location) and val of city 
-
-
-  # desired ====
-  #organized_schools = hash of hashes with arrays
-  #  org schools is first key and value is city name, which is another hash, with value equal to an array of school names 
-  
-
-  # :schools {
-  #   {"flatiron school bk" => {:location => "NYC"},
-  #     "flatiron school" => {:location => "NYC"},
-  #     "dev boot camp" => {:location => "SF"},
-  #     "dev boot camp chicago" => {:location => "Chicago"},
-  #     "general assembly" => {:location => "NYC"},
-  #     "Hack Reactor" => {:location => "SF"}}}
-
-  # :organized_schools {
-  #   {"NYC"=>["flatiron school bk", "flatiron school", "general assembly"],
-  #   "SF"=>["dev boot camp", "Hack Reactor"],
-  #   "Chicago"=>["dev boot camp chicago"]}}
-
-
 
 
 
