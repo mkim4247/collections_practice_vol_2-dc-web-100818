e82b738b142d43869186e3d4e924cd7574a94626
Michael Kim <m.kim4247@gmail.com>
2020-10-13 19:19:22 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,105 +1,22 @@
-# your code goes here
+def begins_with_r(words)
+  words.all? { |word| word[0,1] == 'r' }
+end
 
-def begins_with_r(array)
-  value = true 
-  array.each do |word|
-    if word[0] == "r" 
-      value = true    
-    else 
-      value = false 
-    end 
-  end 
-  value 
-end 
+def contain_a(words)
+  words.filter { |word| 
+    word.each_char.any? { |char| char == 'a'}
+  }
+end
 
-def contain_a(array)
-  arr = [] 
-  array.each do |word|
-    if word.include?("a") 
-      arr << word 
-    end 
-  end 
-  arr 
-end 
-
-def first_wa(array)
-  element = ""
-  array.each do |word|
-    if word[0..1] == "wa"
-      element = word 
-      break 
-    end 
-  end 
-  element 
-end 
+#use filter to find all words that start with 'wa'
+#use first to only return the first filtered element
+def first_wa(words)
+  list = words.select { |word| word[0,2] == 'wa' }
+  return list.first
+end
 
 def remove_non_strings(array)
-  array.delete_if {|element| !(element.is_a? String)}
-end 
+  array.select { |element| element.class == String}  
+end
 
-
-#array = [{:name => "blake"}, {:name => "blake"}, {:name => "ashley"}]
-
-def count_elements(array)
-  count = Hash.new(0)
-  arr = []
-  array.each do |word|
-    count[word] += 1 
-  end
-  count.each do |key, value|
-    key.each do |attribute, name|
-      arr << {:name => name, :count => value}
-    end 
-  end 
-  arr 
-end 
-
-def merge_data(keys, data)
-  arr = []
-  data.each do |hash|
-    hash.each do |name, stats|
-      hash[name][:first_name] = name
-      arr << stats
-    end 
-  end 
-  arr 
-end 
-
-def find_cool(cool)
-  cool_arr = []
-  cool.each do |hash|
-    hash.each do |key, value|
-      if value == "cool"
-        cool_arr << hash 
-      end 
-    end
-  end
-  cool_arr
-end 
-
-def organize_schools(schools)
-  org_schools = {
-    "NYC" => [],
-    "SF" => [],
-    "Chicago" => []
-  }
-  schools.each do |school_name, loc_hash|
-    loc_hash.each do |loc, city|
-      if city == "NYC"
-        org_schools["NYC"] << school_name
-        elsif city == "SF"
-        org_schools["SF"] << school_name
-        elsif city == "Chicago"
-        org_schools["Chicago"] << school_name
-      end
-    end
-  end 
-    org_schools
-end 
-
-
-
-
-
-
-
+def count_elements()
